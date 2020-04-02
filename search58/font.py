5ec19eb693b61e0e0bcb8595499ebdd502792e92
moore.ma <moore.ma@orderplus.com>
2020-04-02 20:58:57 +0800

    commit

--- search58/font.py
+++ search58/font.py
@@ -4,5 +4,11 @@ from fontTools.ttLib import TTFont
 # 加载字体文件：
 font = TTFont('58.woff')
 
-# 转为xml文件：
-font.saveXML('58.xml')
+glyphs = font.getGlyphOrder()[2:]
+tmp_dic = {}
+for num, un_size in enumerate(glyphs):
+    print(un_size,num)
+    font_uni = un_size.replace('uni','0x').lower()
+    print(font_uni)
+    tmp_dic[font_uni] = numprint(tmp_dic)
+
